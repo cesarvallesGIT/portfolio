b8ae587aa9a4666d39173d401e6980c8cad9eb74
Cesar <cesarvalles.git@gmail.com>
2024-01-14 20:07:14 -0600

    music player update

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const getGithubUser = async () => {
       }
     })
 
-    return response.data;
+    return response.data
 } catch (error) {
     console.error('Error fetching user data:', error.message)
     throw error
@@ -26,3 +26,36 @@ const makeGithubUser = async () => {
   githubImgEl.src = githubCallback.avatar_url
 }
 makeGithubUser()
+
+const musicCtnEl = document.getElementById('music-container')
+const musicIconEls = document.querySelectorAll('.music-icons')
+const iframeEl = document.getElementById('iframe')
+const skipBefore = document.getElementById('before')
+const skipAfter = document.getElementById('after')
+const spotifySRC = [
+  "https://open.spotify.com/embed/track/759fzpnRkH3yjHsqBsXwKK?utm_source=generator&theme=0",
+  "https://open.spotify.com/embed/track/4Sav8RLaXMBpTZX6xNPj0K?utm_source=generator",
+  "https://open.spotify.com/embed/track/3gE4eQH3K83Sght0ZLvuBK?utm_source=generator"
+]
+const spotifyColors = ['#282828', 'rgb(81, 128, 116)', 'rgb(83, 83, 83)']
+let musicCount = 0
+skipBefore.addEventListener('click', () => {
+  musicCount -= 1
+  changeSong()
+})
+skipAfter.addEventListener('click', () => {
+  musicCount += 1
+  changeSong()
+})
+const changeSong = () => {
+  musicCount = musicCount < 0 ? spotifySRC.length -1 : musicCount >= spotifySRC.length ? 0 : musicCount
+  musicCtnEl.style.backgroundColor = spotifyColors[musicCount]
+  musicIconEls.forEach((icon, index) => {
+    icon.style.color = spotifyColors[musicCount]
+  })
+  iframeEl.src = spotifySRC[musicCount]
+}
+musicCtnEl.style.backgroundColor = spotifyColors[musicCount]
+musicIconEls.forEach((icon, index) => {
+  icon.style.color = spotifyColors[musicCount]
+})
